5ab9157d3069113e0e19ba6d7a3e9cc98c3f29d7
Notlynoel <leo@notlinoel.com.br>
2022-11-28 16:19:23 -0300

    Adicionando a classe principal ParkingApplication.java

---
+++ src/main/java/br/com/notlinoel/ParkingApplication.java
@@ -0,0 +1,14 @@
+package br.com.notlinoel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApplication.class, args);
+		
+	}
+
+}
